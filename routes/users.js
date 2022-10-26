549e03bbe298e3952ad87c2263dc432f9e9f9a25
akashreddymosra <akashreddymosra@gmail.com>
2022-10-26 17:05:47 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var times=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  times=times+1;
   res.send('respond with a resource');
 });
 
