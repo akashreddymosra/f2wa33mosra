c1165e07a348cb238e3f3901f10e66e1d0c79370
akashreddymosra <akashreddymosra@gmail.com>
2022-10-26 17:34:51 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { pug: 'AkashReddy' });
+  res.render('mydata', { title: 'AkashReddy' });
 });
 
 module.exports = router;
