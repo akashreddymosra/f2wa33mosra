bd7d6566516c265cb367886a4bb62a50069782f3
akashreddymosra <akashreddymosra@gmail.com>
2022-10-26 17:22:50 -0500

    new endpoint

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/',function(req,res,next) {
+    var y = Math.random();
+    var x;
+    x = req.query.x;
+    if(x==undefined)
+    {
+        x=y;
+    }
+
+  res.render('computation', { val: `f(${x}) is ${Math.log(x)}` });
+})
+
+/* GET home page. */
+router.get(`/`,function(req,res,next){
+res.render('computation', { val: `f(${x}) is ${Math.log(x)}` });
+res.end();
+});
+
+module.exports = router;
